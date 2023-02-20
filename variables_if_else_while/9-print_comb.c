9815bbf8c466cea67021c3144fdc5f8c74fedcc3
pizzayolow <ralliard.valentin@gmail.com_>
2023-02-20 07:42:09 -0800

    maj

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *//* more headers goes there */
+
+int main(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+	putchar(n);
+	n++;
+	}
+
+	n = 97;
+
+	while (n <= 102)
+	{
+	putchar(n);
+	n++;
+	}
+
+	putchar('\n');
+	return (0);
+}
