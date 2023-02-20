b572fd3d331457988f0b129d8913bac60a2ce3ce
pizzayolow <ralliard.valentin@gmail.com_>
2023-02-20 06:49:01 -0800

    task7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *//* more headers goes there */
+
+int main(void)
+{
+	int n = 122;
+
+	while (n >= 97)
+	{
+		putchar(n);
+		n--;
+	}
+		putchar ('\n');
+
+	return (0);
+}
