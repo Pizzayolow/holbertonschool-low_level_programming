c5ba476b86b6a0e921f2a2465ccee244ba76ea8d
pizzayolow <ralliard.valentin@gmail.com_>
2023-02-20 04:57:56 -0800

    task2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *//* more headers goes there */
+
+int main(void)
+{
+	int n = 'a';
+
+	while (n <= 'z')
+	{
+		putchar(n);
+		n++;
+	}
+	putchar ('\n');
+
+
+	return (0);
+}
