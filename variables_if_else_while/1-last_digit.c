69fba682ac1d0169044e61e4454050c0ad5a0a14
pizzayolow <ralliard.valentin@gmail.com_>
2023-02-20 04:35:32 -0800

    task1b

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ int main(void)
 	} 
 	else if ((n % 10) == 0)
 	{
-		printf("Last digit of %d and is 0\n", n);
+		printf("Last digit of %d is 0 and is 0\n", n);
 	}
 	else if ((n % 10) < 6 && (n % 10) != 0)
 	{
