7f86ea3588143e50b3eff3e96440b2c4e0fd1f09
pizzayolow <ralliard.valentin@gmail.com_>
2023-02-21 05:54:40 -0800

    task3

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,10 +1,11 @@
 #include "main.h"
-/**_islower - My function for know what is in lowercase or UPPERCASE
- * @parameterx: c are here for the charatcher in ASCII
+/**
+ * _islower - My function for know what is in lowercase or UPPERCASE
+ * @c: c are here for the charatcher in ASCII
  *
  * Return: Return 1 if is an lowercase, return 1 if is an UPPERCASE
  */
-int _islower (int c)
+int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
 		return (1);
