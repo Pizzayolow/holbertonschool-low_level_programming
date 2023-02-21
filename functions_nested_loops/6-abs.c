d14c65e1fd7e16487ed1dc52906813c64c8b4d50
pizzayolow <ralliard.valentin@gmail.com_>
2023-02-21 07:45:55 -0800

    task6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - My function calculate the absolute of a number
+ * @i: i initialize the variable(number)
+ *
+ * Return: Return 0 if not
+ */
+int _abs(int i)
+{
+	if (i < 0)
+	{
+		i = i * -1;
+	}
+		else
+		{
+		}
+
+	return (i);
+}
