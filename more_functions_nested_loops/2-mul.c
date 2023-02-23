2a79437f2eacb653d60b1b100b0693f955f7b9c3
pizzayolow <ralliard.valentin@gmail.com_>
2023-02-23 01:49:45 -0800

    function multiplication

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - Check if the letter is an uppercase or not
+ * @a: The letter
+ * @b: The second parameter
+*
+ * Return: Return 1 if its through 0 and 9
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
