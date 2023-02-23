63c637079f95b50a6deb0164207c510391eb6e83
pizzayolow <ralliard.valentin@gmail.com_>
2023-02-23 15:15:54 -0800

    fizzbuzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int nombre;
+
+	for (nombre = 1; nombre <= 100; nombre++)
+	{
+		if (nombre % 3 == 0 && nombre % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (nombre % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (nombre % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%d ", nombre);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
