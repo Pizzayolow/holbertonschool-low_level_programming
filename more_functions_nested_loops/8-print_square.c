7863fc7515a86faf7417ed214965df8a00b7afa9
pizzayolow <ralliard.valentin@gmail.com_>
2023-02-23 13:45:19 -0800

    le carré

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,11 +1,28 @@
 #include "main.h"
 /**
- * print_diagonal - Check if the letter is an uppercase or not
+ * print_square - Check if the letter is an uppercase or not
  *
- * @n: ma variable
+ * @size: ma variable
  * Return: Return 1 if its through 0 and 9
  */
 void print_square(int size)
 {
+	int colonne;
 
+	int ligne;
+
+	if (size > 0)
+	{
+		for (ligne = 0; ligne < size; ligne++)
+		{
+			for (colonne = 0; colonne < size; colonne++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+
+	}
+	else
+		_putchar('\n');
 }
