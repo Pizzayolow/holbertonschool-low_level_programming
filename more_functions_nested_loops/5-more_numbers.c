af1e8f95d2c40bd8906764757f33688697e0d96e
pizzayolow <ralliard.valentin@gmail.com_>
2023-02-23 04:27:51 -0800

    IceToTheMax

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * more_numbers - Check if the letter is an uppercase or not
+ *
+ * Return: Return 1 if its through 0 and 9
+ */
+void more_numbers(void)
+{
+	int n;
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i >= 10)
+			{
+				_putchar(i / 10 + '0');
+			}
+			_putchar(i % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
