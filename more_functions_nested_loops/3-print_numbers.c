14fc80dee882bb0b21e0d9da6496c17d1ca0dda3
pizzayolow <ralliard.valentin@gmail.com_>
2023-02-23 02:08:06 -0800

    task 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - Check if the letter is an uppercase or not
+*
+ * Return: Return 1 if its through 0 and 9
+ */
+void print_numbers(void)
+{
+int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
