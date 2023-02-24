76d07bdf608249d7dcf8292777c588229f73b55d
pizzayolow <ralliard.valentin@gmail.com_>
2023-02-24 03:37:15 -0800

    triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_triangle - Check if the letter is an uppercase or not
+ *
+ * @size: ma variable
+ * Return: Return 1 if its through 0 and 9
+ */
+void print_triangle(int size)
+{
+	int ligne;
+	int colonne;
+	int diez;
+
+	if (size > 0)
+	{
+		for (ligne = 0; ligne < size; ligne++)
+		{
+			for (colonne = ligne; colonne < size; colonne++)
+			{
+				_putchar(' ');
+			}
+			for (diez = 0; diez <= ligne; diez++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
