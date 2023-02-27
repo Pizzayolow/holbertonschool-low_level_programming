78b6a6cba32882dc2d6a518bce0fc02af03f85a0
pizzayolow <ralliard.valentin@gmail.com_>
2023-02-27 06:36:20 -0800

    task2

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,16 +1,18 @@
+#include "main.h"
 /**
  * _strlen - the function who return the lenght of a string
- * @c: the char
+ * @s: the char
+ * Return: the numberof charachers in this array
  */
 
 int _strlen(char *s)
 {
-	int i= 0;
+	int i = 0;
 
 	while (s[i] != '\0')
 	{
 		i++;
 	}
 
-	return(i);
+	return (i);
 }
