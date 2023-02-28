f147536965ab5e7e017c1415e0901ce4c7a3bd55
pizzayolow <ralliard.valentin@gmail.com_>
2023-02-28 07:56:37 -0800

    task8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - the function who return the lenght of a string
+ * @*a: the char
+ * @n: mmh
+ * Return: the numberof charachers in this array
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	if (n > 0)
+	{
+		while (a[i] < a[n])
+		{
+			printf("%d", a[i]);
+			i++;
+
+			if (a[i] != a[n])
+			{
+				printf(", ");
+			}
+		}
+
+	}
+	printf("\n");
+}
