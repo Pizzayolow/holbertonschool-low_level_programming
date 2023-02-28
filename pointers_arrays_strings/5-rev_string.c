3e7100682d938bd2db2e678a4a1945ea4b2ed609
pizzayolow <ralliard.valentin@gmail.com_>
2023-02-28 05:01:07 -0800

    task5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rev_string - the function who return the lenght of a string
+ * @s: the char
+ * Return: the numberof charachers in this array
+ */
+
+void rev_string(char *s)
+{
+	int compteur = 0;
+	int toto = 0;
+	int temp = 0;
+
+	for (compteur = 0; s[compteur] != '\0'; compteur++)
+	{
+	}
+	compteur = compteur -1;
+	while (toto <= compteur)
+	{
+		temp = s[toto];
+		s[toto] = s[compteur];
+		s[compteur] = temp;
+		compteur --;
+		toto ++;
+	}
+}
