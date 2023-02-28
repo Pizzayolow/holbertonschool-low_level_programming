345259b9acd5a49807ab8796f8be383ca1a9a30f
pizzayolow <ralliard.valentin@gmail.com_>
2023-02-28 14:03:14 -0800

    task 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *_strcpy - the function who return the lenght of a string
+ * @dest: the destination to copy
+ * @src: the source
+ * Return: the string to copy
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
