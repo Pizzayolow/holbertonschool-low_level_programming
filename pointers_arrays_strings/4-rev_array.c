4cdf732b9e9b84d44dfadf2ebf7912b630ca075f
pizzayolow <ralliard.valentin@gmail.com_>
2023-03-01 12:41:17 -0800

    task4

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- * rev_string - the function who return the lenght of a string
- * @s: the char
- * Return: the numberof charachers in this array
+ * reverse_array - the function who return the lenght of a string
+ * @a: sss
+ * @n: the number of elements of my array
  */
 
 void reverse_array(int *a, int n)
 {
 	int temp;
 	int i;
-	
+
 	for (i = 0; i < n; i++)
 	{
 		temp = a[i];
