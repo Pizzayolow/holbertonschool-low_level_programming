05a1b16b9ad3d812b252366fdb6cc0c1add8b7d8
pizzayolow <ralliard.valentin@gmail.com_>
2023-03-01 06:31:47 -0800

    task 2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *_strncat - the function who return the lenght of a string
+ * @dest: the destination to copy
+ * @src: the source
+ * @n: variable
+ * Return: the string to copy
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j = 0;
+
+	for (; j < n && src[j] != '\0'; j++)
+	{
+		dest[j] = src[j];
+	}
+	for (; j < n; j++)
+	{
+	dest[j] = '\0';
+	}
+
+	return (dest);
+}
