e70bab01bcd02e08288859ffdcdee2fb3d608f2e
pizzayolow <ralliard.valentin@gmail.com_>
2023-03-02 07:54:23 -0800

    task0

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _memset- Writee leing to uppercase
+ * @s: the string in main fonction
+ * @b: second parameter
+ * @m: the third parameter
+ * Return: the string to copy
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
