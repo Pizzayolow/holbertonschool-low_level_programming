37e72bc28f46e0871f9feca282c019281bbafd63
pizzayolow <ralliard.valentin@gmail.com_>
2023-03-02 04:53:35 -0800

    task 6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -2,21 +2,21 @@
 #include <stdio.h>
 /**
  * cap_string- Writee leing to uppercase
- * @s: the string in main fonction
+ * @str: the string in main fonction
  * Return: the string to copy
  */
 
 char *cap_string(char *str)
 {
 	int i = 0;
-	char separator[] = {',',';','.','!','?','"','(',')','{','}', 32, 9, '\n'};
+	char s[] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', 32, 9, '\n'};
 	int k;
 
 	while (str[i] != '\0')
 	{
-		for (k = 0; separator[k] != '\0'; k++)
+		for (k = 0; s[k] != '\0'; k++)
 		{
-			if (separator[k - 1] == str[i - 1] && str[i] > 96 && str[i] < 123)
+			if (s[k] == str[i - 1] && (str[i] > 96 && str[i] < 123))
 			{
 			str[i] = str[i] - ('a' - 'A');
 			}
