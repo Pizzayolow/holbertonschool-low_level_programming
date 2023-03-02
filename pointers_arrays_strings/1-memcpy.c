e5f92edac36e07693c341b0c6532b28737dd844a
pizzayolow <ralliard.valentin@gmail.com_>
2023-03-02 08:28:33 -0800

    task1

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,9 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _memcopy- Writee leing to uppercase
- * @s: the string in main fonction
- * @b: second parameter
+ * _memcpy- Writee leing to uppercase
+ * @dest: the string in main fonction
+ * @src: second parameter
  * @n: the third parameter
  * Return: the string to copy
  */
