aa2427bd822e3b00196c5a8126bd381f937c0fe4
pizzayolow <ralliard.valentin@gmail.com_>
2023-03-02 07:11:55 -0800

    task7

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * leet- Writee leing to uppercase
+ * @str: the string in main fonction
+ * Return: the string to copy
+ */
+
+char *leet(char *str)
+{
+	int i;
+	int k;
+	char mintab[] = {'a', 'e', 'o', 't', 'l'};
+	char majtab[] = {'A', 'E', 'O', 'T', 'L'};
+	char numtab[] = {'4', '3', '0', '7', '1'};
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (k = 0; k < 5 ;k++)
+		{
+			if (str[i] == mintab[k] ||  str[i] == majtab[k])
+			{
+				str[i] = numtab[k];
+			}
+		}	
+	}
+	return (str);
+}
