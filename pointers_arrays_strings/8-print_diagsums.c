6f8a1267cfc5ce549504c7e9e7f2b43e99787fa5
pizzayolow <ralliard.valentin@gmail.com_>
2023-03-03 11:43:20 -0800

    atoi

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_diagsums- Writee leing to uppercase
+ * @a: the string in main fonction
+ * @size: the char we must found
+ */
+
+void print_diagsums(int *a, int size)
+{
+
+}
