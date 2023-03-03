3195e5224f4a2172b18ed69182b0e2ea7cdf3590
pizzayolow <ralliard.valentin@gmail.com_>
2023-03-03 00:55:20 -0800

    task2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strchr- Writee leing to uppercase
+ * @s: the string in main fonction
+ * @c: the char we must found
+ * Return: the string to copy
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	return (0);
+}
