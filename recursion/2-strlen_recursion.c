1cef5e506667671c5708a96ce3f20543370d1493
pizzayolow <ralliard.valentin@gmail.com_>
2023-03-06 02:22:16 -0800

    iteration with recursive

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Short description, single line
+ * @s: Description of parameter x
+ * Return: Description of the returned value
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return  (1 + (_strlen_recursion) (s + 1));
+	}
+}
