db23261c01bb0904421af3b7dab44c07fe3c6826
pizzayolow <ralliard.valentin@gmail.com_>
2023-03-06 05:28:16 -0800

    mhh

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,25 +1,29 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - Short description, single line
- * @n: Description of parameter x
- * Return: Description of the returned value
+ * find_root - est for closest int squareroot
+ * @n: number being evaluated
+ * @i: test values being iterated
+ * Return: i if i is the root of n else -1
+ */
+
+int find_root(int n, int i)
+
+{
+	if (i * i == n)
+		return (i);
+	else if (i * i > n)
+		return (-1);
+	return (find_root(n, i + 1));
+}
+/**
+ * _sqrt_recursion - return natural square root of a given number
+ * @n: number being evaluated
+ * Return: square root of n
  */
 
 int _sqrt_recursion(int n)
+
 {
-	if (y < 0)
-	{
-		return (-1);
-	}
-
-	if (y == 0)
-	{
-		return (1);
-	}
-
-	else
-	{
-		return (x * _pow_recursion(x, y - 1));
-	}
+	return (find_root(n, 1));
 }
