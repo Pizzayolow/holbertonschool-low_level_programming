4ee79d8331aa9cb91e57751f82533f384dc0bd60
pizzayolow <ralliard.valentin@gmail.com_>
2023-03-07 02:41:47 -0800

    task0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  * @argv: parameter b
  * Return: Description of the returned value
  */
-int main(int argc __attribute__((unused)), char* argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 
