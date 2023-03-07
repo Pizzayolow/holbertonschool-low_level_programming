a11a0595405afa2bdc179f26e912f48448607ebb
pizzayolow <ralliard.valentin@gmail.com_>
2023-03-07 03:34:18 -0800

    task1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Short description, single line
+ * @argc: Description of parameter x
+ * @argv: parameter b
+ * Return: Description of the returned value
+ */
+int main(int argc, char __attribute__((unused))*argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
