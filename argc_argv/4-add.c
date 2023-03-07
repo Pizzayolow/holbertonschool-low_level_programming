a4e2da63642f2ed9e299f1b73af77c8ff1bc6d12
pizzayolow <ralliard.valentin@gmail.com_>
2023-03-07 05:30:25 -0800

    task4

---
+++ argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - Short description, single line
+ * @argc: Description of parameter x
+ * @argv: parameter b
+ * Return: Description of the returned value
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int k;
+	int sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{	
+		for (k = 0; argv[i][k]; k++)
+		{
+			if (!isdigit(argv[i][k]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
