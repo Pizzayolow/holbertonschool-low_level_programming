38bc76b28f5a51d97dc0a0507aacc14d14a540d6
pizzayolow <ralliard.valentin@gmail.com_>
2023-03-13 13:32:48 -0700

    tableau bi dimmensionnel

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * alloc_grid - Short description, single line
+ * @width: Description of parameter x
+ * @height: second char
+ * Return: ints
+ */
+int **alloc_grid(int width, int height)
+{
+	int **tableau;
+	int i;
+	int j;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	tableau = malloc(height * sizeof(int *));
+	if (tableau == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		tableau[i] = malloc(width * sizeof(int));
+		if (tableau[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(tableau[i]);
+			}
+			free(tableau[j]);
+			return (NULL);
+		}
+	}
+	return (tableau);
+}
