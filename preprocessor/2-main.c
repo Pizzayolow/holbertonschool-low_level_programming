b7c4414c888ac51b05b9c6f2d519e6bde026cc53
pizzayolow <ralliard.valentin@gmail.com_>
2023-03-15 06:40:15 -0700

    task2

---
+++ preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Short description, single line
+ * Return: always sucess
+ */
+
+int main(void)
+{
+	printf("Name of the file is = %s\n", __FILE__);
+return (0);
+}
