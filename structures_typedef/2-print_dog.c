a2115bfc8b39c9452658fc0538a67e8c0f518f4f
pizzayolow <ralliard.valentin@gmail.com_>
2023-03-20 05:35:16 -0700

    toto

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -14,16 +14,26 @@
 
 void print_dog(struct dog *d)
 {
+	if (d == NULL)
+	{
+		return;
+	}
 	if (d->name == NULL)
 	{
 		printf("(nil)");
+	}
+	else
+	{
+		printf("Name: %s\n", d->name);
 	}
 	if (d->owner == NULL)
 	{
 		printf("(nil)");
 	}
+	else
+	{
+		printf("Owner: %s\n", d->owner);
+	}
 
-	printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
 }
