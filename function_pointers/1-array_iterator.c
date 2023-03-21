483acf3694166f2b1166b8afc213865ea5544235
pizzayolow <ralliard.valentin@gmail.com_>
2023-03-21 03:24:31 -0700

    task1

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -11,9 +11,9 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	long unsigned int i = 0;
+	unsigned long int i = 0;
 
-	if (action == NULL)
+	if (array == NULL)
 	{
 		return;
 	}
