2d33e44928e47e76961235259b86c5e83215f84b
pizzayolow <ralliard.valentin@gmail.com_>
2023-03-21 04:46:16 -0700

    task2

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - Short description, single line
+ * @array: Description of parameter x
+ * @size: s
+ * @cmp: s
+ * Return: int
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
