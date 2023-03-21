722f99a966ff945b94d9333e9982875fae5b7c8f
pizzayolow <ralliard.valentin@gmail.com_>
2023-03-21 07:18:42 -0700

    task3 l'enfer

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -12,14 +12,17 @@
 int main(int argc, char *argv[])
 {
 	int a = 0;
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[3]);
+	int num1;
+	int num2;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	
 	if (get_op_func(argv[2]) == NULL)
 	{
 		printf("Error\n");
