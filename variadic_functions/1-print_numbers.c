9fd6487925881efe0bbcc8d864718355d199a889
pizzayolow <ralliard.valentin@gmail.com_>
2023-03-22 02:39:26 -0700

    task1

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - Short description, single line
+ * @separator: Description of parameter x
+ * @n: int
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list(count); //crée une variable de type variadique
+	va_start(count, n); // ta liste aura le nombre de "n"
+	int i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(count, int));
+		if (i != n - 1 && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(count);
+}
