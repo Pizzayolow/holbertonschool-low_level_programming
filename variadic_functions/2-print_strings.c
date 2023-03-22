f8a13cd41dcd30a6197c6043ef74d2ee0b044aad
pizzayolow <ralliard.valentin@gmail.com_>
2023-03-22 03:05:08 -0700

    task2

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - Short description, single line
+ * @separator: Description of parameter x
+ * @n: int
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list(count);
+	unsigned int i = 0;
+
+	va_start(count, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%s", va_arg(count, char*));
+		if (i != n - 1 && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(count);
+}
