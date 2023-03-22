a1c5445fc3e7c28639ca01b3d5a08286ba1b8034
pizzayolow <ralliard.valentin@gmail.com_>
2023-03-22 03:17:00 -0700

    task2

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -16,7 +16,16 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(count, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%s", va_arg(count, char*));
+		char *arg = va_arg(count, char*);
+
+		if (arg == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+		{
+			printf("%s", arg);
+		}
 		if (i != n - 1 && separator != NULL)
 		{
 			printf("%s", separator);
