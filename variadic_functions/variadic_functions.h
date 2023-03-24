c066c0439d8776315efa60548e798350efd19ce8
pizzayolow <ralliard.valentin@gmail.com_>
2023-03-24 13:39:02 -0700

    hmm

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,5 +1,8 @@
-#ifndef VARIADIC
-#define VARIADIC
+#ifndef _Variadic_
+#define _Variadic_
+
+#include <stdio.h>
+#include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
@@ -10,5 +13,4 @@ typedef struct my_struct
 	char *op;
 	void (*f)(va_list);
 } my_struct;
-
 #endif
