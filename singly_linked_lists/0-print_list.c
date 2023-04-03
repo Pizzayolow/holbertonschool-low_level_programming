1e16b60fa44b49f607e9cd96df8bda2a782c454c
pizzayolow <ralliard.valentin@gmail.com_>
2023-04-03 02:07:50 -0700

    first single list with a count

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+/**
+ * print_list - print the size of a list
+ * @h: string - the h
+ * Return: the size of the list
+ *
+ */
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			h = h->next;
+		}
+		else
+		{
+			printf("[%d]%s\n", h->len, h->str);
+			h = h->next;
+		}
+		i++;
+	}
+	return (i);
---
+++ singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+The README's Holberton week of Singly linked lists
