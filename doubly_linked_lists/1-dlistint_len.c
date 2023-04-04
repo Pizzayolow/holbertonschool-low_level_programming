5018a5348a44205ccdc319e536fe9469e0ac0b7a
pizzayolow <ralliard.valentin@gmail.com_>
2023-04-04 03:46:34 -0700

    task1

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * dlistint_len - Short description, single line
+ * @h: Description of parameter x
+ (*
+ * Return: The total of the nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int i = 0;
+
+	while (h != NULL)
+	{
+		int k = h->n;
+
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
