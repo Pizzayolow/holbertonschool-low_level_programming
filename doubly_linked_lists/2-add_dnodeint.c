8946d128480e9a33a1f54ab454cedb715407fa51
pizzayolow <ralliard.valentin@gmail.com_>
2023-04-04 04:22:48 -0700

    task2, like single

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+ * add_dnodeint - Short description, single line
+ * @n: The number given by the main
+ * @head: the struct
+ (*
+ * Return: The total of the nodes
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *newnode = malloc(sizeof(dlistint_t));
+
+	if (newnode == NULL)
+	{
+		return (NULL);
+	}
+
+	if (head == NULL)
+	{
+		newnode->n = n;
+		newnode->prev = NULL;
+		newnode->next = NULL;
+		*head = newnode;
+
+		return (*head);
+	}
+	newnode->n = n;
+	newnode->prev = NULL;
+	newnode->next = *head;
+	*head = newnode;
+
+	return (*head);
+}
