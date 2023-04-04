5d1eec2afa13d13cd41d2b66d622799a11865e69
pizzayolow <ralliard.valentin@gmail.com_>
2023-04-04 02:33:51 -0700

    task0, count double linked list

---
+++ doubly_linked-lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+/**
+ * print_dlistint - Short description, single line
+ * @h: Description of parameter x
+ (*
+ * Return: The total of the nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int i = 0;
+
+	while (h != NULL)
+	{
+		int k = h->n;
+
+		printf("%d\n", k);
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
