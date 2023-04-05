fad24904d1384e708b51ec03e2590b792510be63
pizzayolow <ralliard.valentin@gmail.com_>
2023-04-05 04:03:50 -0700

    task4

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_dlistint - Short description, single line
+ * @head: the struct
+ (*
+ * Return: The total of the nodes
+ */
+void free_dlistint(dlistint_t *head)
+
+{
+	if (head->next != NULL)
+	{
+		free_dlistint(head->next);
+	}
+
+	free(head);
+
+}
