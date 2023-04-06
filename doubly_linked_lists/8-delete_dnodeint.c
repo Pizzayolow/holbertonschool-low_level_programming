36c6059e531155c992c377751d74007233f7831e
pizzayolow <ralliard.valentin@gmail.com_>
2023-04-06 02:33:28 -0700

    task8

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -13,7 +13,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *tmp = *head;
 	dlistint_t *tmp2 = NULL;
 
-	if (head == NULL)
+	if (*head == NULL)
 	{
 		return (-1);
 	}
@@ -21,6 +21,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if ((*head)->next == NULL)
 	{
 		free(*head);
+		*head = NULL;
 		return (1);
 	}
 
