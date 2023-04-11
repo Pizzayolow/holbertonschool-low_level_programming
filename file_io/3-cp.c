2de36c683d83467b6eae145eeae4bc14a4a9cdf0
pizzayolow <ralliard.valentin@gmail.com_>
2023-04-11 14:07:09 -0700

    task 3 copy!

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -12,7 +12,7 @@ int main(int argc, char **argv)
 
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to");
+		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	file_from = open(argv[1], O_RDONLY);
@@ -24,7 +24,7 @@ int main(int argc, char **argv)
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_to == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[2]);
+		dprintf(2, "Error: Can't write from file %s\n", argv[2]);
 		return (98);
 	}
 	while ((n = read(file_from, buffer, 1024)) != 0)
@@ -35,7 +35,17 @@ int main(int argc, char **argv)
 		}
 		write(file_to, buffer, n);
 	}
-	close(file_from);
-	close(file_to);
+	if (close(file_from) == -1)
+	{
+		dprintf(2, "Error: Can't close from file %d\n", file_from);
+		return (100);
+	}
+	if (close(file_to) == -1)
+	{
+		dprintf(2, "Error: Can't close from file %d\n", file_to);
+		return (100);
+	}
 	return (0);
 }
+
+
