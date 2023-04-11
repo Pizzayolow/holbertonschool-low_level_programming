93ec70cd4f5dbceafb6b6fe5cf114e4cd832812d
pizzayolow <ralliard.valentin@gmail.com_>
2023-04-11 03:42:16 -0700

    task 1, create and wrint in file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 		return (0);
 	}
 
-	fd = open(filename, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR | O_TRUNC);
+	fd = open(filename, O_RDWR | O_CREAT | S_IRUSR | S_IWUSR | O_TRUNC);
 	if (fd == -1)
 	{
 		return (-1);
