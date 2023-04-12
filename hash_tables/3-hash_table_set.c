cbab9a7cb4370a662ff48afc2994bb8b9942694a
pizzayolow <ralliard.valentin@gmail.com_>
2023-04-12 05:55:17 -0700

    task4

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -27,7 +27,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht->array[index] == NULL)
 	{
 		ht->array[index] = newnode;
-		return (0);
+		return (1);
 	}
 	newnode->next = ht->array[index];
 	ht->array[index] = newnode;
