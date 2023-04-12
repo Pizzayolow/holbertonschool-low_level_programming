74a32f2b4099edd25e755cf4fd12d6134c7c2978
pizzayolow <ralliard.valentin@gmail.com_>
2023-04-12 04:05:19 -0700

    task2

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+/**
+ * key_index - Short description, single line
+ * @key: Description of parameter x
+ * @size: the size
+ * Return: Description of the returned value
+*/
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+{
+	unsigned long int = 0;
+
+	n = hash_djb2(key);
+	n = n % size;
+
+	return (n);
+}
+
